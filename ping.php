f4ac0b66e58f6bb478d801f1bd8bfd35bb2297c8
Reasonable-Grape2698 <dyl.secure@gmail.com>
2024-12-08 17:08:18 +1100

    Update ping.php

--- ping.php
+++ ping.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['ip'])){
 $host = $_POST['ip'];
-$pingtime = shell_exec('ping -qc1 google.com 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5" ms":"FAIL") }'');
+$pingtime = shell_exec(ping -qc1 google.com 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5" ms":"FAIL") }');
 echo $pingtime
 }
 ?>
